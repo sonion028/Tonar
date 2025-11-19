81b8293c5a5e46e83fa932f7e7d94969438e15ab
zhourenbo.ssse <zhourenbo.ssse@bytedance.com>
2025-11-19 14:43:34 +0800

    build: 在 rollup 配置中添加 react/jsx-runtime 为外部依赖

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
       fileName: (format, entryName) => `${entryName}.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
